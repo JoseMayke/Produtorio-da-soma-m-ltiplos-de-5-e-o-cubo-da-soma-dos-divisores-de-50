8d7a43f82253040c089c290e0db7f97766968ab2
JoseMayke <maykekero@gmail.com>
2021-05-01 00:09:16 -0300

    Enviando codigo

---
+++ main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+somaMultiplo5(x,y)
+ | x == y && mod x 5 == 0 = x
+ | x == y = 0
+ | x < y && mod x 5 == 0 = x + somaMultiplo5(x+1,y)
+ | x < y = somaMultiplo5(x+1,y)
+ | otherwise = somaMultiplo5(y,x)
+
+somaDivisor50(x,y)
+ | x == y && mod 50 x == 0 = x
+ | x == y = 0
+ | x < y && mod 50 x == 0 = x + somaDivisor50(x+1,y)
+ | x < y = somaDivisor50(x+1,y)
+ | otherwise = somaDivisor50(y,x)
+
+questao2(i,f) = somaMultiplo5(i,f) * (somaDivisor50(i,f) ^ 3)
